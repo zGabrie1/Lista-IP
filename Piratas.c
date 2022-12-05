8c246ceb8142a614297a28bc0e1d39b425221223
SoaresGabriel <gabriel980500@gmail.com>
2022-12-05 18:10:15 -0300

    Tentar depois

--- Piratas.c
+++ Piratas.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
 int main() {
-    
+    int acorde, tonica, terca, quinta;
+    scanf("%i", &acorde);
+    if(acorde > 100) {
+        tonica = acorde%100;
+        terca = acorde%10;
+        quinta = acorde;
+    }
+    printf("%i %i %i", tonica, terca, quinta);
 }
